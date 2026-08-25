7f54d012010ca1abe01cc6a4e37d5ea59d55b6f0
Andrew Patton <atpatton97@gmail.com>
2026-08-25 15:29:39 +0100

    Switched to play events from studio banks

--- FMODSandbox.cpp
+++ FMODSandbox.cpp
@@ -1,93 +1,96 @@
-#include <iostream>
-#include <conio.h>
-
 #include <chrono>
+#include <conio.h>
+#include <filesystem>
+#include <iostream>
+#include <map>
 #include <thread>
 
 #include <fmod.hpp>
-
 #include <fmod_errors.h>
+#include <fmod_studio.hpp>
+
+using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
 
 bool succeededOrWarn(const std::string& message, FMOD_RESULT result)
 {
-    if (result != FMOD_OK) {
+    if (result != FMOD_OK)
+    {
         std::cerr << message << ": " << result << " " << FMOD_ErrorString(result) << std::endl;
         return false;
     }
     return true;
 }
 
-FMOD_RESULT F_CALLBACK channelGroupCallback(FMOD_CHANNELCONTROL* channelControl,
-    FMOD_CHANNELCONTROL_TYPE controlType, FMOD_CHANNELCONTROL_CALLBACK_TYPE callbackType,
-    void* commandData1, void* commandData2)
+std::map<std::string, FMOD::Studio::Bank*> loadBanksFromFolder(FMOD::Studio::System* studioSystem, std::string folderPath)
 {
-    std::cout << "Callback called for " << controlType << std::endl;
-    return FMOD_OK;
+    std::map<std::string, FMOD::Studio::Bank*> banksKeyedByName;
+    std::string bankFileExtension(".bank");
+    for (const auto& dirEntry : recursive_directory_iterator(folderPath))
+    {
+        if (dirEntry.path().extension() != bankFileExtension)
+            continue;
+
+        FMOD::Studio::Bank* bank;
+        FMOD_RESULT result = studioSystem->loadBankFile(dirEntry.path().string().c_str(), FMOD_STUDIO_LOAD_BANK_NORMAL, &bank);
+        if (succeededOrWarn(std::string("Failed to load bank at path ").append(dirEntry.path().string()), result))
+        {
+            banksKeyedByName[dirEntry.path().string()] = bank;
+            std::cout << "Successfully loaded bank at path " << dirEntry.path() << std::endl;
+        }
+    }
+
+    return banksKeyedByName;
 }
 
-void playAudioAsync(FMOD::System* system, const char* audioPath, FMOD::ChannelGroup* channelGroup)
+void playAudioAsync(FMOD::Studio::System* studioSystem, const char* eventPath)
 {
-    std::cout << "playing: " << audioPath << std::endl;
-
-    FMOD::Sound* sound = nullptr;
-    system->createSound(audioPath, FMOD_DEFAULT, nullptr, &sound);
-
-    // Play the sound.
-    FMOD::Channel* channel = nullptr;
-    auto result = system->playSound(sound, nullptr, false, &channel);
-    if (!succeededOrWarn("FMOD: Failed to play sound", result))
+    FMOD::Studio::EventDescription* eventDescription = nullptr;
+    FMOD_RESULT result = studioSystem->getEvent(eventPath, &eventDescription);
+    if (!succeededOrWarn(std::string("Failed to find event at path ").append(eventPath), result))
+    {
         return;
+    }
 
-    // Assign the channel to the group.
-    result = channel->setChannelGroup(channelGroup);
-    if (!succeededOrWarn("FMOD: Failed to set channel group on", result))
+    FMOD::Studio::EventInstance* eventInstance = nullptr;
+    result = eventDescription->createInstance(&eventInstance);
+    if (!succeededOrWarn(std::string("Failed to create instance of event at path ").append(eventPath), result))
+    {
         return;
+    }
 
-    // Set a callback on the channel.
-    channel->setCallback(&channelGroupCallback);
-    if (!succeededOrWarn("FMOD: Failed to set callback for sound", result))
-        return;
+    eventInstance->start();
 
-    bool isPlaying = false;
-    do {
+    FMOD_STUDIO_PLAYBACK_STATE state;
+    do
+    {
+        studioSystem->update();
+        eventInstance->getPlaybackState(&state);
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    } while (state != FMOD_STUDIO_PLAYBACK_STOPPED);
 
-        channel->isPlaying(&isPlaying);
-
-        system->update();
-    } while (isPlaying);
-
-    // Clean up.
-    sound->release();
+    eventInstance->release();
 }
 
 int main(int argc, char* argv[])
 {
-    FMOD::System* system = nullptr;
+    FMOD::Studio::System* studioSystem = nullptr;
 
     // Create the main system object.
-    FMOD_RESULT result = FMOD::System_Create(&system);
+    FMOD_RESULT result = FMOD::Studio::System::create(&studioSystem);
     if (!succeededOrWarn("FMOD: Failed to create system object", result))
         return 1;
 
     // Initialize FMOD.
-    result = system->init(512, FMOD_INIT_NORMAL, nullptr);
+    result = studioSystem->initialize(512, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, nullptr);
     if (!succeededOrWarn("FMOD: Failed to initialise system object", result))
         return 1;
 
-    // Create the channel group.
-    FMOD::ChannelGroup* channelGroup = nullptr;
-    result = system->createChannelGroup("inGameSoundEffects", &channelGroup);
-    if (!succeededOrWarn("FMOD: Failed to create in-game sound effects channel group", result))
-        return 1;
+    std::map<std::string, FMOD::Studio::Bank*> banksKeyedByName = loadBanksFromFolder(studioSystem, "Assets/FMODBanks/");    
 
-    // Create the sound.
-    std::thread playAudioAsyncTask(playAudioAsync, system, "Assets/Alarm01.wav", channelGroup);
-
+    std::thread playAudioAsyncTask(playAudioAsync, studioSystem, "event:/Ambience/Forest");
     playAudioAsyncTask.join();
 
-    channelGroup->release();
-    system->release();
+    studioSystem->release();
 
     std::cout << "Hello World!\n";
 
